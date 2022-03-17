f2fa65e1238a8614f341e3de323867e7fbb4c381
DESKTOP-JS4T63V\Isaac <isaacgrc94@gmail.com>
2022-03-17 16:27:12 +0100

    add filters and plot

--- app.R
+++ app.R
@@ -1,20 +1,21 @@
 library(shiny)
 library(shinyjs)
 library(DT)
+library(bslib)
+library(ggplot2)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
+  #theme = bs_theme(version = 4, bootswatch = "darkly"),
   shinyjs::useShinyjs(),
   
    # Application title
    titlePanel("INVENTARIO"),
    sidebarLayout(
      sidebarPanel(
-       radioButtons("dist", "Totales:",
-                    c("Format" = "forma",
-                      "Edicion" = "edition",
-                      "Year" = "years")),
-       DT::dataTableOutput("mytable")
+       DT::dataTableOutput("formato"),
+       DT::dataTableOutput("edicion"),
+       DT::dataTableOutput("year")
      ),
      mainPanel(
        tabsetPanel(
@@ -28,65 +29,91 @@ ui <- fluidPage(
                               ".csv")
                   ),
                   verbatimTextOutput("carga")),
-         tabPanel("Estadistica", "Graficos")
+         tabPanel("Estadistica", "Graficos",
+                  radioButtons("dist", "Totales:",
+                               c("Format" = "forma",
+                                 "Edicion" = "edition",
+                                 "Year" = "years")),
+                  DT::dataTableOutput("mytable"),
+                  plotOutput("estadistica"))
        )
      )
    )
 )
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-  
-  filtro <- reactive({
+  leerpelis <- reactive({
     pelis <- read.csv("InventarioPeliculas.csv", header = TRUE, sep = ";", dec = ".", stringsAsFactors=FALSE, fileEncoding="latin1")
     
-    s = input$mytable_rows_selected
+    return(pelis)
+  })
+  
+  filtropelis <- reactive({
     
-    if (length(s)) {
-      switch (input$dist,
-              "forma" = {
-                formatos.df <- as.data.frame(table(pelis$Formato))
-                colnames(formatos.df) = c("Formato", "Cantidad")
-                formatos <- formatos.df
-                
-                formatoOK <- as.character(formatos[s, "Formato"])
-                
-                pelisFormato <- pelis[which(pelis$Formato == formatoOK), ]
-                pelis <- pelisFormato
-              },
-              "edition" = {
-                ediciones.df <- as.data.frame(table(pelis$Edicion))
-                colnames(ediciones.df) = c("Edicion", "Cantidad")
-                ediciones <- ediciones.df
-                
-                edicionOK <- as.character(ediciones[s, "Edicion"])
-                
-                pelisEdicion <- pelis[which(pelis$Edicion == edicionOK), ]
-                pelis <- pelisEdicion
-              },
-              "years" = {
-                years.df <- as.data.frame(table(pelis$Year))
-                colnames(years.df) = c("Year", "Cantidad")
-                years <- years.df
-                
-                yearOK <- as.character(years[s, "Year"])
-                
-                pelisYear <- pelis[which(pelis$Year == yearOK), ]
-                pelis <- pelisYear
-              }
-              )
+    pelis <- read.csv("InventarioPeliculas.csv", header = TRUE, sep = ";", dec = ".", stringsAsFactors=FALSE, fileEncoding="latin1")
+    
+    #guardamos la seleccion de cada tabla
+    seleccionformato = input$formato_rows_selected
+    seleccionedicion = input$edicion_rows_selected
+    seleccionyear = input$year_rows_selected
+    
+    #si no hay ninguno no se filtra nada 
+    if (length(seleccionformato) | length(seleccionedicion) | length(seleccionyear)) {
+      #si hay alguno marcado, hay que ir aplicando los filtros al total de pelis
       
-      return(pelis) 
+      #Obtenemos seleccion
+      formatos.df <- as.data.frame(table(pelis$Formato))
+      colnames(formatos.df) = c("Formato", "Cantidad")
+      formatos <- formatos.df
       
-      #El siguiente paso seria poder filtrar aplicando los filtros a la vez
+      formatoOK <- as.character(formatos[seleccionformato, "Formato"])
+      
+      if(!length(formatoOK)) formatoOK <- ""
+      
+      ediciones.df <- as.data.frame(table(leerpelis()$Edicion))
+      colnames(ediciones.df) = c("Edicion", "Cantidad")
+      ediciones <- ediciones.df
+
+      edicionOK <- as.character(ediciones[seleccionedicion, "Edicion"])
+      
+      if(!length(edicionOK)) edicionOK <- ""
+      
+      years.df <- as.data.frame(table(leerpelis()$Year))
+      colnames(years.df) = c("Year", "Cantidad")
+      years <- years.df
+
+      yearOK <- as.character(years[seleccionyear, "Year"])
+      
+      if(!length(yearOK)) yearOK <- ""
+      #Fin obtener seleccion
+      
+      #guardamos la seleccion
+      seleccion <- c(formatoOK, edicionOK, yearOK)
+      
+      if (seleccion[1:0] != "") {
+        pelis <- pelis[which(pelis$Formato == formatoOK), ]
+      }
+      
+      if (seleccion[2:2] != "") {
+        pelis <- pelis[which(pelis$Edicion == edicionOK), ]
+      }
+      
+      if (seleccion[3:3] != "") {
+        pelis <- pelis[which(pelis$Year == yearOK), ]
+      }
+      
+    }else{
+      leerpelis()
+    }
+    
+    return(pelis)
+      
       # hacer mas bonito el sitio con imagenes y tal
       #controlar la carga de fichero (permitir add nuevas pelis, controlar duplicados)
       #add graficos bonitos
       #intentar hacer login y control de usuarios
       
-    }
-    else {
-      pelis <- read.csv("InventarioPeliculas.csv", header = TRUE, sep = ";", dec = ".", stringsAsFactors=FALSE, fileEncoding="latin1")
-    }
+    #}
   })
   
   cargafichero <- reactive({
@@ -126,12 +153,12 @@ server <- function(input, output) {
   })
   
   output$tablePelis = DT::renderDataTable({
-    filtro()
+    filtropelis()
   })
 
 #Uso de la libreria DT para formatear tabla de salida (Paginacion, busqueda, ordenacion)
     
-   output$mytable = DT::renderDataTable(selection = 'single', {
+   output$mytable = DT::renderDataTable({
      pelis <- read.csv("InventarioPeliculas.csv", header = TRUE, sep = ";", dec = ".", stringsAsFactors=FALSE, fileEncoding="latin1")
      
      dist <- switch(input$dist,
@@ -146,12 +173,46 @@ server <- function(input, output) {
                   years = {year.df <- as.data.frame(table(pelis$Year))
                   colnames(year.df) = c("Year", "Cantidad")
                   pelis <- year.df})
-
    })
    
    output$carga = renderPrint({
      cargafichero()
 
+   })
+   
+   output$formato = DT::renderDataTable(selection = 'single', {
+
+     formatos.df <- as.data.frame(table(leerpelis()$Formato))
+     colnames(formatos.df) = c("Formato", "Cantidad")
+     pelis <- formatos.df
+     
+   })
+   
+   output$edicion = DT::renderDataTable(selection = 'single', {
+
+     edicion.df <- as.data.frame(table(leerpelis()$Edicion))
+     colnames(edicion.df) = c("Edicion", "Cantidad")
+     pelis <- edicion.df
+   })
+   
+   output$year = DT::renderDataTable(selection = 'single', {
+
+     year.df <- as.data.frame(table(leerpelis()$Year))
+     colnames(year.df) = c("Year", "Cantidad")
+     pelis <- year.df
+   })
+   
+   output$estadistica <- renderPlot({
+     s = input$mytable_rows_selected
+     
+     formatos.df <- as.data.frame(table(leerpelis()$Formato))
+     colnames(formatos.df) = c("Formato", "Cantidad")
+     formatos <- formatos.df
+     
+     if (length(s)){
+       plot(formatos[s, , drop = FALSE])
+       formatos[]
+     } 
    })
 }
 
